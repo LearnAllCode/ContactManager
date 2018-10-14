81cf70b0390c50bb8b81dbc4d1cdf094b2b8aef2
restores <ferraro2ndbase@gmail.com>
2018-10-14 15:54:04 -0400

    Changing state

--- contactmanager/src/components/Contact.js
+++ contactmanager/src/components/Contact.js
@@ -4,16 +4,32 @@ import "./contact.css";
 
 // rcc to create
 class Contact extends Component {
+  state = {
+    showContactInfo: false
+  };
+
   render() {
-    // Destructor
     const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
-        <ul className="list-group">
-          <li className="list-group-item">Email: {email}</li>
-          <li className="list-group-item">Phone: {phone}</li>
-        </ul>
+        <h4>
+          {name}{" "}
+          <i
+            onClick={() =>
+              this.setState({ showContactInfo: !this.state.showContactInfo })
+            }
+            className="fas fa-sort-down"
+          >
+            {" "}
+          </i>
+        </h4>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {email}</li>
+            <li className="list-group-item">Phone: {phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
